75a4aa11c4643f1d05d799f521b263ae2d0de169
Richard Pineo <Richard.E.Pineo@geaerospace.com>
2025-12-01 18:50:30 -0800

    Initial Commit

---
+++ AOC2025/AOC2025App.swift
@@ -0,0 +1,17 @@
+//
+//  AOC2025App.swift
+//  AOC2025
+//
+//  Created by Richard Pineo on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AOC2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
